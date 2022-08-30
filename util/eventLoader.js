6ab92d449a1f98320585b7dcc2c8e56beeee322d
EminTr <31457544+EminTr@users.noreply.github.com>
2022-08-30 03:56:01 +0300

    eventLoader.js

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,4 +1,4 @@
 const emirhansarac = (event) => require(`../events/${event}`);
 module.exports = client => {
-  client.on('message', emirhansarac('message'));
+  client.on('message', Fix('message'));
 };
